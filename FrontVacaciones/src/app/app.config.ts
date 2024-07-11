0b523cbc25b63c551bcc0fb2adb8c2bf8c81d30b
David Julian Muñoz Norberto <143279636+davidjulian7@users.noreply.github.com>
2024-07-11 16:05:40 -0600

    correción error
    error NG02801

--- FrontVacaciones/src/app/app.config.ts
+++ FrontVacaciones/src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes), 
-    provideHttpClient(withInterceptorsFromDi())
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptorsFromDi(), withFetch()), // Añadir withFetch aquí
+    provideClientHydration()
   ]
 };
-provideHttpClient(withInterceptorsFromDi(), withFetch());
